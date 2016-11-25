a85c8b19ef788ccb3b9e59e1d81afe7d465cd681
shakervc <shakervc@yahoo.com>
2016-11-25 12:44:37 -0500

    Added back the changes of yesterday that were lost Thank God for local history.

--- SMS_TypeScript/components/directives/courseDirective.ts
+++ SMS_TypeScript/components/directives/courseDirective.ts
@@ -9,9 +9,8 @@ interface IStudent  extends ng.IScope
 class CustomCtrl {
     root: string;
     course: String;
-    students: Array<{"id": String}>;
+    students: {};
     addChecked: boolean;
-    nextId: number;
     // For present application, I need to inject only $scope. I am leaving the others to remind me that these also can
     // injected.
     static $inject = ['$scope', '$http', '$element'];
@@ -21,57 +20,83 @@ class CustomCtrl {
     {
         $scope.studentController = this;
         this.course = $scope.course;
-        this.root = 'http://localhost:3000/' + this.course + "/";
+
+        this.root = 'http://localhost:4567/' + this.course + "/";
         this.getData();
     }
+// 11/20/2106 Bottom Line
+// Node server sends data and we take the success branch
+// Stack server sends data but we take the error branch. I don't know how to set the results in that branch
+// self is flaky too.
+/*
+  The JSON payload is processed by angular BEFORE the then branch is reached. If there is any error (such as a single quote in the payload,
+  myError is called instead of mySuccess.
+  I couldn't get the $.ajax to work -- I expected it to work. I didn't troubleshoot it.
+ $.ajax({
+ url: this.root,
+ method: 'GET'
+ }).then( function mySucces(response) {
 
+ var result = $.parseJSON(data); // Use this version for the simple Java server
+ var result = data;  // Use this version for the node server
+ //result = [{"fname": "Venkat", "lname": "Shaker", "id": 1}];  // Use this to input test data
+ self.students = result;
+ self.$scope.$apply();
+
+ */
     private getData() {
         // Without the next statement, the function below cannot access the 'this' of the outer scope.
         // self is of type Window. this is of type CustomCtrl. How do things work then?
         var self = this;
+        console.log("Going to get data");
         ////self: CustomCtrl = this; Does not work. I don't know how to do this in TypeScript
         //self: Window = this; Does not work
-        $.ajax({
-            url: this.root,
-            method: 'GET'
-        }).then(function(data) {
-            // The compiler first looks for self in the present function scope. Not finding it, it next looks in the
-            // most immediate outer scope, namely that of getData. It finds it here.
-            self.students = data;
-            self.$scope.$apply();
-            self.nextId = 0;
-            for (let student of self.students) {
-                if (Number(student.id) > self.nextId) self.nextId = Number(student.id);
-            }
-            self.nextId += 1;
+
+        this.$http({
+            method : "GET",
+            url : this.root
+        }).then(function mySucces(response) {
+            console.log("success branch");
+            // How to get rid of the TypeScript warning?
+            self.students = response.data;
+            // Leaving the next line in causes an Error: [$rootScope:inprog]. This used to be necessary. Is this necessary after updates?
+            //self.$scope.$apply(); // Getting an error on this line while processing each object. However, the data is still displayed correctly
+        }, function myError(response) {
+            console.log("Error processing JSON from server");
         });
     }
     private add(student) {
+
         if (typeof student != 'undefined') {
             var self = this;
             this.addChecked = false;
-            $.ajax(this.root, {
-                method: 'POST',
-                data: {
-                    fname: student.fname,
-                    lname: student.lname,
-                    id: this.nextId
+            this.$http( {
+                method: "POST",
+                url : this.root,
+                data: { fname: student.fname,
+                        lname: student.lname,
+                        id: 20
                 }
-            }).then(function (data) {
-
-                console.log(data);
+            } ).then(function mySucces(response) {
+                //console.log(self.students);
+                console.log(response.data);
                 student.fname = "";
                 student.lname = "";
-                self.getData();
+                self.students = response.data;
+                //self.$scope.$apply();
             });
         }
     }
     private update(student1, index) {
         var self = this;
         if (typeof student1 != 'undefined') {
-            $.ajax(this.root + this.students[index].id, {
+            this.$http( {
                 method: 'PUT',
-                data: student1
+                data: {
+                    newdata: student1,
+                    index: index
+                },
+                url: this.root + this.students[index].id
             }).then(function (data) {
                 student1.fname = ""
                 student1.lname = "";
@@ -82,16 +107,31 @@ class CustomCtrl {
     private delete(index) {
         if (confirm("Are you sure you want to delete this student?")) {
             var self = this;
-            this.root + this.students[index].id;
-            $.ajax(this.root + this.students[index].id, {
-                method: 'DELETE'
-            }).then(function(data) {
-                console.log(data);
-                self.getData();
+            this.$http( {
+                method: "DELETE",
+                url: this.root + this.students[index].id,
+
+                data: {
+                    index: index
+                }
+            } ).then(function mySucces(response) {
+                //console.log(self.students);
+                console.log(response.data);
+                self.students = response.data;
+                //self.$scope.$apply();
             });
         }
     }
 }
-// Looks like the registration of controller is not needed
-//angular.module('myApp').controller("CustomCtrl", CustomCtrl);
+/*
+ Looks like the registration of controller, i.e., angular.module('myApp').controller("CustomCtrl", CustomCtrl);,  is not needed
+
+ TODO:
+
+ . There is an error while processing each object in the array: Error: [$rootScope:inprog]  http://stackoverflow.com/questions/22733422/angularjs-rootscopeinprog-inprogress-error.
+   Got rid of the error by commenting out the $apply() line.
+ . Getting getData to work with $.ajax method
+ . Storing things in GitHub
+ . List of questions for the Davids
+*/
 
